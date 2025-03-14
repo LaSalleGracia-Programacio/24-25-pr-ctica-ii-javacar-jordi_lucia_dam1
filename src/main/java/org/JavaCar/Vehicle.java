960a22c3c87bb9c49f28d7429a184d46c504eace
JordiFA <jordi.fernandez@gracia.lasalle.cat>
2025-03-14 12:42:55 +0100

    atributs Vehicle

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -1,4 +1,12 @@
 package org.JavaCar;
 
 public class Vehicle {
+    private String matricula;
+    private String marca;
+    private String model;
+    private double preuBase;
+    private Motor motor;
+    private Roda roda;
+    private String etiquetaAmbiental;
+
 }
