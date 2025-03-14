72d6d64def0712f84ad4abff6539a96847c85334
JordiFA <jordi.fernandez@gracia.lasalle.cat>
2025-03-14 13:02:24 +0100

    static gestor

--- src/main/java/org/JavaCar/GestorLloguers.java
+++ src/main/java/org/JavaCar/GestorLloguers.java
@@ -3,11 +3,11 @@ package org.JavaCar;
 import java.util.List;
 
 public class GestorLloguers {
-    public double calcularIngressos(List<Vehicle> vehicles, int dies){
+    public static double calcularIngressos(List<Vehicle> vehicles, int dies){
         return 0;
     }
 
-    public List<Vehicle> filtrarPerPreu(List<Vehicle> vehicles, double preuMax){
+    public static List<Vehicle> filtrarPerPreu(List<Vehicle> vehicles, double preuMax){
         return vehicles;
     }
 }
