f7345383c50cd250c5d4368400f4f90b53f4bcdf
JordiFA <jordi.fernandez@gracia.lasalle.cat>
2025-03-14 12:54:00 +0100

    GestorLloguers

--- src/main/java/org/JavaCar/GestorLloguers.java
+++ src/main/java/org/JavaCar/GestorLloguers.java
@@ -1,4 +1,13 @@
 package org.JavaCar;
 
+import java.util.List;
+
 public class GestorLloguers {
+    public double calcularIngressos(List<Vehicle> vehicles, int dies){
+        return 0;
+    }
+
+    public List<Vehicle> filtrarPerPreu(List<Vehicle> vehicles, double preuMax){
+        return vehicles;
+    }
 }
