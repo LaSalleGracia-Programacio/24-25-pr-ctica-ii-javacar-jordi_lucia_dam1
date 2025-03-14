178f5b6537b8a7afda4761640f7acf4c5513b467
JordiFA <jordi.fernandez@gracia.lasalle.cat>
2025-03-14 12:49:34 +0100

    vehicle abstracte

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -1,12 +1,12 @@
 package org.JavaCar;
 
-public class Vehicle {
-    private String matricula;
-    private String marca;
-    private String model;
-    private double preuBase;
-    private Motor motor;
-    private Roda roda;
-    private String etiquetaAmbiental;
+public abstract class Vehicle {
+    protected String matricula;
+    protected String marca;
+    protected String model;
+    protected double preuBase;
+    protected Motor motor;
+    protected Roda roda;
+    protected String etiquetaAmbiental;
 
 }
