f44b813286cabbca1ec54b57747bb699642fdcd9
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2025-03-19 12:14:47 +0100

    motors

--- src/main/java/org/JavaCar/Main.java
+++ src/main/java/org/JavaCar/Main.java
@@ -7,22 +7,26 @@ public class Main {
     public static void main(String[] args) {
         tiposMotor();
     }
-    public static List<String> tiposMotor(){
+
+    // Mètode per retornar els tipus de motors
+    public static List<String> tiposMotor() {
         List<String> tipos = new ArrayList<String>();
         tipos.add("Gasolina Euro3");
         tipos.add("Gasolina Euro4");
         tipos.add("Gasolina Euro5");
         tipos.add("Gasolina Euro6");
+        tipos.add("Diesel Euro4");
+        tipos.add("Diesel Euro5");
         tipos.add("Diesel Euro6");
         tipos.add("Híbrid endollable");
+        tipos.add("Elèctric híbrid endollable (PHEV)");
         tipos.add("Híbrid no endollable (HEV)");
-        tipos.add("PHEV");
+        tipos.add("Elèctrics de bateria (BEV)");
+        tipos.add("Elèctrics d'autonomia estesa (REEV)");
         tipos.add("Gas natural");
         tipos.add("Gas natural comprimit (GNC)");
         tipos.add("Gas liquat del petroli (GLP)");
         tipos.add("Sense distinció");
-
-        //BEV, REEV
         return tipos;
     }
 
