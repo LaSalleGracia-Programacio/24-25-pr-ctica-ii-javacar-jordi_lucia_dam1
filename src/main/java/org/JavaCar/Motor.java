55063d5263a7a731e3eb3816236a8a2c8632f788
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2025-03-19 12:54:25 +0100

    motor get set

--- src/main/java/org/JavaCar/Motor.java
+++ src/main/java/org/JavaCar/Motor.java
@@ -3,4 +3,33 @@ package org.JavaCar;
 public class Motor {
     private String tipus;
     private float potencia;
+
+    // Constructor de la clase Motor
+    public Motor(String tipus, float potencia) {
+        this.tipus = tipus;
+        this.potencia = potencia;
+    }
+
+    // Getter para obtener el tipo de motor
+    public String getTipus() {
+        return tipus;
+    }
+
+    // Setter para establecer el tipo de motor
+    public void setTipus(String tipo) {
+        this.tipus = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "Motor: " + tipus;
+    }
+
+    public float getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(float potencia) {
+        this.potencia = potencia;
+    }
 }
