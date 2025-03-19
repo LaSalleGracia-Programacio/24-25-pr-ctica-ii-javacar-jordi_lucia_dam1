90af2c700610b49d4cba3b2573ad142625391df6
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2025-03-19 16:11:13 +0100

    gestor lloguers

--- src/main/java/org/JavaCar/GestorLloguers.java
+++ src/main/java/org/JavaCar/GestorLloguers.java
@@ -1,13 +1,33 @@
 package org.JavaCar;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GestorLloguers {
-    public static double calcularIngressos(List<Vehicle> vehicles, int dies){
-        return 0;
+
+    // Mètode per calcular els ingressos totals de lloguers
+    public static double calcularIngressosTotals(List<Vehicle> vehicles, int dies) {
+        double ingressosTotals = 0.0;
+
+        // Iterar sobre la llista de vehicles i sumar el preu de cada vehicle
+        for (Vehicle vehicle : vehicles) {
+            ingressosTotals += vehicle.calcularPreu(dies);
+        }
+
+        return ingressosTotals;
     }
 
-    public static List<Vehicle> filtrarPerPreu(List<Vehicle> vehicles, double preuMax){
-        return vehicles;
+    // Mètode per filtrar vehicles segons el preu màxim
+    public static List<Vehicle> filtrarPerPreu(List<Vehicle> vehicles, double preuMax) {
+        List<Vehicle> vehiclesFiltrats = new ArrayList<>();
+
+        // Iterar sobre la llista de vehicles i afegir els que compleixen el criteri
+        for (Vehicle vehicle : vehicles) {
+            if (vehicle.calcularPreu(1) <= preuMax) {  // El càlcul es fa per un dia per comparar el preu base
+                vehiclesFiltrats.add(vehicle);
+            }
+        }
+
+        return vehiclesFiltrats;
     }
 }
