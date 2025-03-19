b22bf1e0cd176a6f264239bd5e48719583ea4776
Jordi Fernández Arlegui <jordi.fernandez@gracia.lasalle.cat>
2025-03-19 13:00:25 +0100

    marca getset

--- src/main/java/org/JavaCar/Motor.java
+++ src/main/java/org/JavaCar/Motor.java
@@ -4,18 +4,15 @@ public class Motor {
     private String tipus;
     private float potencia;
 
-    // Constructor de la clase Motor
     public Motor(String tipus, float potencia) {
         this.tipus = tipus;
         this.potencia = potencia;
     }
 
-    // Getter para obtener el tipo de motor
     public String getTipus() {
         return tipus;
     }
 
-    // Setter para establecer el tipo de motor
     public void setTipus(String tipo) {
         this.tipus = tipo;
     }
--- src/main/java/org/JavaCar/Roda.java
+++ src/main/java/org/JavaCar/Roda.java
@@ -3,4 +3,28 @@ package org.JavaCar;
 public class Roda {
     private String marca;
     private float diametre;
+
+
+    public Roda(String marca, float diametre) {
+        this.marca = marca;
+        this.diametre = diametre;
+    }
+
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+
+    public float getDiametre() {
+        return diametre;
+    }
+
+    public void setDiametre(float diametre) {
+        this.diametre = diametre;
+    }
 }
